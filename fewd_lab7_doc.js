911be6e2c96328fa180c58bb72aeadba39aa4a09
gregdu91 <55101978+gregdu91@users.noreply.github.com>
2020-11-09 16:56:00 +0100

    1

---
+++ fewd_lab7_doc.js
@@ -0,0 +1,70 @@
+// Grégoire Courtois 22875 github link : https://github.com/gregdu91/fewd_lab_7
+
+//3)
+var firstName = 'Grégoire';
+
+//4)
+var favouriteColor = 'blue';
+
+//5)
+var favouriteFood = 'meat';
+var city = 'Barcelona'
+
+//6)
+x = 'Toe'
+
+//7)
+var y = 'Hardy';
+
+//8)
+// console.log() show us the value of our var
+console.log(firstName)
+console.log(favouriteColor)
+console.log(favouriteFood)
+
+//10)
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops sosmall. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase =' is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two ='orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + one +" " + two+" " + three+" " + four+" " + five+" " + six +" "+ seven+ " " + endingPhrase)
+
+//12)
+length =  7;
+
+//13)
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+function myAlphabetLength() {
+
+    return myAlphabet.length
+
+};
+
+console.log(myAlphabetLength());
+
+//14)
+var x = 5 , y = 10, z =15 
+
+x+y = 15
+y-x = -5
+x*y = 50
+y/x = 2
+x % 2 = 0.1
+x++ = 5
+x-- = 5
+
+
+
+
+
+
+
+
+
+
